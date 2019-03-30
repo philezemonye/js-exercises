a12bada3e44372ea098eae31124910499c6816ea
Phil Ezemonye <philezemonye@gmail.com>
2019-03-30 13:11:31 +0000

    Completed A-objects-intro/exercise-part-0.js

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,12 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  brand: "leveno",
+  screenSeize: "12 inches",
+  colour: "black",
+  touchScreen: false,
+  touchPad: true,
+  ram: "4gb",
+  hardDisk: 500
+};
