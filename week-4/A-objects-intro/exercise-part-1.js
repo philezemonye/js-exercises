ccb7615bbaa600bf9330c2a2bd26e3aa59368dbb
Phil Ezemonye <philezemonye@gmail.com>
2019-03-30 13:12:55 +0000

    Completed A-objects-intro/exercise-part-1.js

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,3 +6,34 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+var table = {
+  legs: 4,
+  colour: "gray",
+  shape: "rectangle",
+  isFoldAble: true
+};
+var banana = {
+  colour: "yellow",
+  isRipe: true,
+  size: "4 inches"
+};
+var house = {
+  numOfFloors: 2,
+  numOfWindows: 7,
+  hasGarden: true,
+  hasCloakRoom: false
+};
+var classroom = {
+  numStudent: 20,
+  hasClassStarted: true,
+  numOfTables: 8,
+  numOfChairs: 20,
+  className: "code your future",
+  hasProjector: true
+};
+var bag = {
+  colour: "blue",
+  type: "laptop bag",
+  numCompartments: 2,
+  isBig: false
+};
